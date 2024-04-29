fb721c97608862ce38fcb8f8df7fa745e66a8581
incnutrition <askus@incnutritionllc.com>
2024-04-29 12:47:35 -0700

    complete page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@
 import { useEffect } from "react";
 import { useState } from 'react'
 import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
+import axios from 'axios';
 
 export default function Home() {
   const [data, setData] = useState<any[]>([
@@ -12,7 +13,7 @@ export default function Home() {
     { name: 'cat5', column: ['RA', 'DEC', 'Name'], row: 1000, filesize: '100000 bytes' },
   ]);
   const [order, setOrder] = useState<any[]>([
-    'cat1', 'cat2', 'cat3', 'cat4', 'cat5'
+    
   ]);
   const [file, setFile] = useState<any>(null);
   
@@ -21,8 +22,32 @@ export default function Home() {
   };
 
   const handleSubmit = async () => {
-    // Handle file upload
+    const formData = new FormData();
+    formData.append('file', file);
+    formData.append('name', file.name.replace('.csv', ''));
+    try {
+      let response = await axios.post('http://localhost:4000/upload', formData);
+    } catch (error) {
+      console.error('Error uploading file:', error);
+    }
+    fetchData();
   };
+
+  const [loading, setLoading] = useState<boolean>(false);
+
+  const fetchData = async () => {
+    let r = await axios.get('http://localhost:4000/data');
+    setData(r.data);
+    let o = []
+    for (let i = 0; i < r.data.length; i++) {
+      o.push(r.data[i].name);
+    }
+    setOrder(o);
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
 
   const onDragEnd = (result: any) => {
     // Reorder the list
@@ -36,6 +61,23 @@ export default function Home() {
 
   const merge = async () => {
     // Merge the database
+    setLoading(true);
+    let formData = new FormData();
+    formData.append('order', JSON.stringify(order));
+    formData.append('outname', 'output.csv');
+    let r = await axios.post('http://localhost:4000/merge', formData, {
+      responseType: 'blob'
+    });
+    const url = window.URL.createObjectURL(new Blob([r.data]));
+    const link = document.createElement('a');
+    link.href = url;
+    link.setAttribute('download', 'output.csv');
+    document.body.appendChild(link);
+    link.click();
+
+    // Clean up
+    window.URL.revokeObjectURL(url);
+    setLoading(false);
   };
 
   return (
@@ -72,7 +114,8 @@ export default function Home() {
         <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm mt-5">
           <div className="flex flex-row">
             <h1 className="text-3xl font-bold mb-8 mr-8">Order & Merge</h1>
-            <button onClick={merge} className="border border-solid border-gray-500 px-4 py-2 rounded-lg h-full">MERGE</button>
+            {!loading && <button onClick={merge} className="border border-solid border-gray-500 px-4 py-2 rounded-lg h-full">MERGE</button>}
+            {loading && <button className="border border-solid border-gray-500 px-4 py-2 rounded-lg h-full">Loading...</button>}
           </div>
           <DragDropContext onDragEnd={onDragEnd}>
             <Droppable droppableId="droppable">
